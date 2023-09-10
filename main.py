49ff84b0ceb710080565c91d38be0439de0b7f46
Ryan <ryanchen980401@gmail.com>
2023-09-10 12:52:56 +0800

    finish

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ def square(number):
     :param number: int or float
     :return: int or float, the square of the input number
     """
-    pass  # Implement your solution here
-
+      # Implement your solution here
+    return number*number
 
 def reverse_string(s):
     """
@@ -13,8 +13,11 @@ def reverse_string(s):
     :param s: str
     :return: str, the reversed string
     """
-    pass  # Implement your solution here
-
+    # Implement your solution here
+    reverse=""
+    for i in range(len(s)-1,-1,-1):
+        reverse+=i
+    return reverse
 
 def is_prime(n):
     """
@@ -22,7 +25,14 @@ def is_prime(n):
     :param n: int
     :return: bool, True if the number is prime, otherwise False
     """
-    pass  # Implement your solution here
+  # Implement your solution here
+    if n<=1:
+        return False
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    return True
+
 
 
 def factorial(n):
@@ -31,7 +41,11 @@ def factorial(n):
     :param n: int
     :return: int, the factorial of the input number
     """
-    pass  # Implement your solution here
+    # Implement your solution here
+    temp=1
+    for i in range(n,0,-1):
+        temp=temp*i
+    return temp
 
 def find_maximum(lst):
     """
@@ -39,7 +53,12 @@ def find_maximum(lst):
     :param lst: list of int
     :return: int, the maximum number in the list
     """
-    pass  # Implement your solution here
+    # Implement your solution here
+    max=lst[0]
+    for i in lst:
+        if i>max:
+            max=i
+    return max
 
 def odd_or_even(n):
     """
@@ -47,8 +66,11 @@ def odd_or_even(n):
     :param n: int
     :return: str, "Odd" or "Even"
     """
-    pass  # Implement your solution here
-
+    # Implement your solution here
+    if n%2==0:
+        return "Even"
+    if n%2!=0:
+        return "Odd"
 def is_palindrome(s):
     """
     This function takes a string `s` and returns `True` if the string is a palindrome, and `False` otherwise. 
@@ -57,7 +79,12 @@ def is_palindrome(s):
     :param s: str
     :return: bool, `True` if the string is a palindrome, `False` otherwise.
     """
-    pass  # Implement your solution here
+    # Implement your solution here
+    s=s.lower()
+    backwards=""
+    for i in range(len(s)):
+        backwards+=s[len(s)-i-1]
+    return s==backwards
 
 def find_gcd(a, b):
     """
@@ -67,5 +94,14 @@ def find_gcd(a, b):
     :param b: int
     :return: int, the greatest common divisor of `a` and `b`.
     """
-    pass  # Implement your solution here
-
+    # Implement your solution here
+    divisor=0
+    if a<b:
+        for i in range(1,a+1,1):
+            if a%i==0 and b%i==0:
+                divisor=i
+    if b<a:
+        for i in range(1,b+1,1):
+            if a%i==0 and b%i==0:
+                divisor=i
+    return divisor
